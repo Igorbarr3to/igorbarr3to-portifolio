41b645e2e023b9834beccbfd3161ccb1519b7dc4
igorbarr3to <igorbarreto1110@gmail.com>
2025-05-20 10:47:14 -0400

    setup inicial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Typewriter from "@/components/type-writer";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center ">
+      <h1> <Typewriter /> </h1>
+    </div>
+  );
+}
---
+++ components/type-writer.tsx
@@ -0,0 +1,37 @@
+// components/Typewriter.tsx
+'use client'; // se estiver usando App Router
+
+import { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+
+export default function Typewriter() {
+  const el = useRef<HTMLSpanElement | null>(null);
+  const typed = useRef<Typed | null>(null);
+
+  useEffect(() => {
+    typed.current = new Typed(el.current!, {
+      strings: ['&lt;igorbarr3to/&gt;'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 1000,
+      loop: true,
+    });
+
+    return () => {
+      typed.current?.destroy();
+    };
+  }, []);
+
+  return (
+    <span
+      ref={el}
+      style={{
+        fontFamily: 'monospace',
+        fontSize: '24px',
+        borderRight: '2px solid black',
+        whiteSpace: 'nowrap',
+        display: 'inline-block',
+      }}
+    />
+  );
+}
